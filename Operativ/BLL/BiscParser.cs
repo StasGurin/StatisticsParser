dabe09163aebc006f8a8cd7c36b483c3066c7cf5
StasGurin <azgard0308@gmail.com>
2015-10-09 14:19:29 +0300

    Fix #1

---
+++ Operativ/BLL/BiscParser.cs
@@ -0,0 +1,43 @@
+﻿using HtmlAgilityPack;
+using System.Text;
+using Operativ.Models;
+using Operativ.Extensions;
+
+namespace Operativ.BLL
+{
+    public class BiscParser
+    {
+        static int parseLineNumber = 3;
+        static Month record = new Month();
+
+        public static Month Parse(string year)
+        {
+            var webGet = new HtmlWeb
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = UTF8Encoding.GetEncoding("windows-1251"),
+            };
+            var link = "http://www.ukrstat.gov.ua/operativ/operativ" + year + "/ct/";
+            var docBisc = webGet.Load(link + "bisc/bisc_u/bisc_" + year + "u.htm");//get html-page for parse
+            var bodyNode = docBisc.DocumentNode.SelectSingleNode("//table[@class ='MsoNormalTable']");
+            if (bodyNode != null)
+            {
+                var node = bodyNode.SelectSingleNode(".//tr[" + parseLineNumber + "]");
+                if (node != null)
+                {
+                    var nodeMounth = node.SelectSingleNode(".//td[1]");
+                    var nodePercent = node.SelectSingleNode(".//td[2]"); //get percetns from html page
+                    if ((nodeMounth != null) && (nodePercent != null))
+                    {  
+                        //get id (format:yearmonth)
+                        record.YearMonth = YearMonthExtensions.ToYearMonth(year, nodeMounth.InnerText);
+                        record.Percent = nodePercent.InnerText.Trim();
+
+                    }
+                }
+            }
+            parseLineNumber++;
+            return record;
+        }
+    }
+}
---
+++ Operativ/BLL/IscParser.cs
@@ -0,0 +1,49 @@
+﻿using HtmlAgilityPack;
+using System.Text;
+using Operativ.Models;
+using Operativ.Extensions;
+
+namespace Operativ.BLL
+{
+    public class IscParser
+    {
+        static int parseLineNumber = 2;
+        static Month record = new Month();
+
+        public static Month Parse(string year)
+        {
+            
+            var webGet = new HtmlWeb
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = UTF8Encoding.GetEncoding("windows-1251"),
+            };
+            var link = "http://www.ukrstat.gov.ua/operativ/operativ" + year + "/ct/";
+            var docIsc = webGet.Load(link + "is_c/isc_u/isc" + year + "m_u.html");
+            if (year == "2014") docIsc = webGet.Load(link + "is_c/isc_u/isc" + year + "m_u_.html");//get html-page for parse
+            var bodyNode = docIsc.DocumentNode.SelectSingleNode("//table[@class ='MsoNormalTable']");
+            if (bodyNode != null)
+            {
+                var nodeMounth = bodyNode.SelectSingleNode(".//tr[1]");
+                var nodePercent = bodyNode.SelectSingleNode(".//tr[2]");
+                if ((nodeMounth != null) && (nodePercent != null))
+                {
+                    var node = nodeMounth.SelectSingleNode(".//td[" + parseLineNumber + "]");
+                    if (node != null)
+                    {
+                        //get id (format:yearmonth)
+                        record.YearMonth = YearMonthExtensions.ToYearMonth(year, node.InnerText);
+
+                    }
+                    node = nodePercent.SelectSingleNode(".//td[" + parseLineNumber + "]");
+                    if (node != null)
+                    {
+                        record.Percent = node.InnerText.Trim();
+                    }
+                }
+            }
+            parseLineNumber++;
+            return record;
+        }
+    }
+}
