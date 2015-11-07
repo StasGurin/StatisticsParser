8ffb289d78362e5c55136a79582db110de023e55
azakanych@malkosua.com <@idjogHets8%>
2015-11-07 22:29:11 +0200

    Added BaseController

---
+++ Operativ/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using MongoDB.Driver;
+using Operativ.BLL;
+using Operativ.Models;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Operativ.Controllers
+{
+    public abstract class BaseController : ApiController
+    {
+        protected async Task Parse(string id, Parser parser, string collectionName)
+        {
+            var host = ConfigurationManager.AppSettings["MongoDBHost"];
+            var dbName = ConfigurationManager.AppSettings["MongoDBName"];
+            var collection = new MongoClient(host).GetDatabase(dbName).GetCollection<Month>(collectionName);
+
+            foreach(var month in parser.Parse(id))
+            {
+                var filter = Builders<Month>.Filter.Eq(m => m.YearMonth, month.YearMonth);
+                await collection.ReplaceOneAsync(filter, month, new UpdateOptions { IsUpsert = true });
+            }
+        }
+    }
+}
